4cf3a892e25b9c9a02cd81711867dfb78ae277e3
wanthinnn <thienlai159@gmail.com>
2026-06-03 14:27:01 +0700

    update lib version from 2 to 3

--- src/cpp/include/hybrid-cp-abe.h
+++ src/cpp/include/hybrid-cp-abe.h
@@ -29,7 +29,7 @@ namespace HybridCPABE {
     constexpr size_t GCM_IV_SIZE = 12;          // 96-bit theo NIST SP 800-38D
     constexpr size_t AES_KEY_SIZE = 32;         // 256-bit AES key
     constexpr uint8_t FORMAT_VERSION = 0x01;    // Ciphertext format version
-    const char* const LIB_VERSION = "2.0.0";
+    const char* const LIB_VERSION = "3.0.0";
     const char* const DEFAULT_KEY_FORMAT = "Base64";
 }
 
